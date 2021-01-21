386ff8e89b983cf7b0e89835c54867ddb3700e4c
B00344208 <B00344208@sudentmail.uws.ac.uk>
2021-01-21 20:45:40 +0000

    Bloom is implemented
    bloom is succesfully implemented for the bullets, object mtl files are changed to have 4 letters infront of them as it wouldnt work otherwise for me

--- Lab/Main.cpp
+++ Lab/Main.cpp
@@ -48,6 +48,10 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 void processInput(GLFWwindow* window);
 
+void renderSphere();
+
+void renderQuad();
+
 
 
 // settings
@@ -58,6 +62,9 @@ const unsigned int SCR_HEIGHT = 1000;
 
 bool blinn = true;
 
+bool bloom = true;
+
+float exposure = 1.0f;
 
 
 
@@ -140,7 +147,7 @@ glm::vec3 BulletPosition[10];
 
 float BulletRotation[10];
 
-float BulletSpeed = 35.0f;
+float BulletSpeed = 20.0f;
 
 bool wasShoot[10];
 
@@ -150,7 +157,7 @@ bool canShoot = true;
 
 int ReamainingLifeSpawn[10];
 
-int BulletLifeSpawn = 20;
+int BulletLifeSpawn = 60;
 
 
 
@@ -256,9 +263,13 @@ int main()
 
     Shader ToonTest("Shaders/ToonV3.vs", "Shaders/ToonV3.fs");
 
+    Shader bloomshader("Shaders/7.bloom.vs", "Shaders/7.bloom.fs");
 
+    Shader shaderLight("Shaders/7.bloom.vs", "Shaders/7.light_box.fs");
 
+    Shader shaderBlur("Shaders/7.blur.vs", "Shaders/7.blur.fs");
 
+    Shader shaderBloomFinal("Shaders/7.bloom_final.vs", "Shaders/7.bloom_final.fs");
 
 
 
@@ -273,6 +284,84 @@ int main()
     Model Rock("resources/objects/rock/rock.obj");
 
     Model Bullet("resources/objects/bullet/bullet.obj");
+
+
+
+    // configure (floating point) framebuffers
+    // ---------------------------------------
+    unsigned int hdrFBO;
+    glGenFramebuffers(1, &hdrFBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+    // create 2 floating point color buffers (1 for normal rendering, other for brightness threshold values)
+    unsigned int colorBuffers[2];
+    glGenTextures(2, colorBuffers);
+    for (unsigned int i = 0; i < 2; i++)
+    {
+        glBindTexture(GL_TEXTURE_2D, colorBuffers[i]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);  // we clamp to the edge as the blur filter would otherwise sample repeated texture values!
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        // attach texture to framebuffer
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, colorBuffers[i], 0);
+    }
+    // create and attach depth buffer (renderbuffer)
+    unsigned int rboDepth;
+    glGenRenderbuffers(1, &rboDepth);
+    glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SCR_WIDTH, SCR_HEIGHT);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
+    // tell OpenGL which color attachments we'll use (of this framebuffer) for rendering 
+    unsigned int attachments[2] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1 };
+    glDrawBuffers(2, attachments);
+    // finally check if framebuffer is complete
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "Framebuffer not complete!" << std::endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    // ping-pong-framebuffer for blurring
+    unsigned int pingpongFBO[2];
+    unsigned int pingpongColorbuffers[2];
+    glGenFramebuffers(2, pingpongFBO);
+    glGenTextures(2, pingpongColorbuffers);
+    for (unsigned int i = 0; i < 2; i++)
+    {
+        glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[i]);
+        glBindTexture(GL_TEXTURE_2D, pingpongColorbuffers[i]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // we clamp to the edge as the blur filter would otherwise sample repeated texture values!
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, pingpongColorbuffers[i], 0);
+        // also check if framebuffers are complete (no need for depth buffer)
+        if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+            std::cout << "Framebuffer not complete!" << std::endl;
+    }
+
+
+
+    // lighting info
+    // -------------
+    // position
+    std::vector<glm::vec3> lightPositions;
+    lightPositions.push_back(glm::vec3(BulletPosition->x, BulletPosition->y, BulletPosition->z));
+    // color
+    std::vector<glm::vec3> lightColors;
+    lightColors.push_back(glm::vec3(15.0f, 0.0f, 0.0f));
+
+
+
+    // shader configuration
+    // --------------------
+    bloomshader.use();
+    bloomshader.setInt("diffuseTexture", 0);
+    shaderBlur.use();
+    shaderBlur.setInt("image", 0);
+    shaderBloomFinal.use();
+    shaderBloomFinal.setInt("scene", 0);
+    shaderBloomFinal.setInt("bloomBlur", 1);
 
 
 
@@ -307,8 +396,6 @@ int main()
         // -----      
 
         processInput(window);
-
-
 
 
 
@@ -508,7 +595,7 @@ int main()
 
                     {
 
-                        float distance = BulletCurrentSpeed[i] * deltaTime;
+                        float distance = BulletCurrentSpeed[i] * (deltaTime);
 
                         float dx = (float)(distance * sin(BulletRotation[i] - M_PI / 2));
 
@@ -546,6 +633,99 @@ int main()
 
 
 
+        // 1. render scene into floating point framebuffer
+        // -----------------------------------------------
+        glBindFramebuffer(GL_FRAMEBUFFER, hdrFBO);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glm::mat4 projection1 = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view1 = camera.GetViewMatrix();
+        glm::mat4 model1 = glm::mat4(1.0f);
+
+        i = 9;
+
+        //draw bullets
+        shaderLight.use();
+        shaderLight.setMat4("projection", projection1);
+        shaderLight.setMat4("view", view1);
+
+        while (i > 0)
+        {
+
+            if (wasShoot[i] == true)
+            {
+
+
+
+
+
+                if (ReamainingLifeSpawn[i] > 0)
+                {
+
+
+
+
+                    model1 = glm::mat4(1.0f);
+
+                    model1 = glm::translate(model1, BulletPosition[i]);
+
+                    model1 = glm::scale(model1, glm::vec3(0.8f, 0.8f, 0.8f));     //scale
+
+                    model1 = glm::rotate(model1, BulletRotation[i], glm::vec3(0.0f, 1.0f, 0.0f));
+
+                    shaderLight.setMat4("model", model1);
+
+                    shaderLight.setVec3("lightColor", lightColors[0]);
+
+                    renderSphere();
+
+                }
+
+                else
+                {
+                    wasShoot[i] = false;
+                }
+
+            }
+            i--;
+
+        }
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+
+
+
+
+        // 2. blur bright fragments with two-pass Gaussian Blur 
+                    // --------------------------------------------------
+        bool horizontal = true, first_iteration = true;
+        unsigned int amount = 20;
+        shaderBlur.use();
+        for (unsigned int i = 0; i < amount; i++)
+        {
+            glBindFramebuffer(GL_FRAMEBUFFER, pingpongFBO[horizontal]);
+            shaderBlur.setInt("horizontal", horizontal);
+            glBindTexture(GL_TEXTURE_2D, first_iteration ? colorBuffers[1] : pingpongColorbuffers[!horizontal]);  // bind texture of other framebuffer (or scene if first iteration)
+            renderQuad();
+            horizontal = !horizontal;
+            if (first_iteration)
+                first_iteration = false;
+        }
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        // 3. now render floating point color buffer to 2D quad and tonemap HDR colors to default framebuffer's (clamped) color range
+        // --------------------------------------------------------------------------------------------------------------------------
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        shaderBloomFinal.use();
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, colorBuffers[0]);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, pingpongColorbuffers[!horizontal]);
+        shaderBloomFinal.setInt("bloom", bloom);
+        shaderBloomFinal.setFloat("exposure", exposure);
+        renderQuad();
+        
+        glClear(GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
          // set uniforms
 
         shaderSingleColor.use();
@@ -579,19 +759,21 @@ int main()
         glStencilMask(0xFF);
 
 
+        
+        shader.use();
+        shader.setMat4("projection", projection);
+        shader.setMat4("view", view);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, playerPosition); // translate it down so it's at the center of the scene
+        model = glm::rotate(model, playerRotation, glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(0.01f, 0.01f, 0.01f));	// it's a bit too big for our scene, so scale it down
+        shader.setMat4("model", model);
+        Rocket.Draw(shader);
 
 
-    shader.use();
-    shader.setMat4("projection", projection);
-    shader.setMat4("view", view);
-    model = glm::mat4(1.0f);
-    model = glm::translate(model, playerPosition); // translate it down so it's at the center of the scene
-    model = glm::rotate(model, playerRotation, glm::vec3(0.0f, 1.0f, 0.0f));
-    model = glm::scale(model, glm::vec3(0.01f, 0.01f, 0.01f));	// it's a bit too big for our scene, so scale it down
-    shader.setMat4("model", model);
-    Rocket.Draw(shader);
 
         i = 9;
+
 
 
         //draw rocks
@@ -663,71 +845,16 @@ int main()
             i--;
         }
 
-        i = 9;
+        
+
+        
 
 
-
-        //draw bullets
-
-        while (i > 0)
-
-        {
-
-            if (wasShoot[i] == true)
-
-            {
-
-
-
-
-
-                if (ReamainingLifeSpawn[i] > 0)
-
-                {
-
-                    shader.use();
-
-
-
-                    model = glm::mat4(1.0f);
-
-                    model = glm::translate(model, BulletPosition[i]);
-
-                    model = glm::scale(model, glm::vec3(0.8f, 0.8f, 0.8f));     //scale
-
-                    model = glm::rotate(model, BulletRotation[i], glm::vec3(0.0f, 1.0f, 0.0f));
-
-                    shader.setMat4("model", model);
-
-                    Bullet.Draw(shader);
-
-
-
-                }
-
-                else
-
-                {
-
-                    wasShoot[i] = false;
-
-                }
-
-
-
-            }
-
-            i--;
-
-        }
 
         i = 9;
 
+        
 
-
-
-
-       
         //Rocks stencil buffer
         if (loaded == true)//stencil
         {
@@ -808,8 +935,6 @@ int main()
     return 0;
 
 }
-
-
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
@@ -954,3 +1079,130 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
     //camera.ProcessMouseScroll(yoffset);
 }
 
+// renders (and builds at first invocation) a sphere
+// -------------------------------------------------
+unsigned int sphereVAO = 0;
+unsigned int indexCount;
+void renderSphere()
+{
+    if (sphereVAO == 0)
+    {
+        glGenVertexArrays(1, &sphereVAO);
+
+        unsigned int vbo, ebo;
+        glGenBuffers(1, &vbo);
+        glGenBuffers(1, &ebo);
+
+        std::vector<glm::vec3> positions;
+        std::vector<glm::vec2> uv;
+        std::vector<glm::vec3> normals;
+        std::vector<unsigned int> indices;
+
+        const unsigned int X_SEGMENTS = 64;
+        const unsigned int Y_SEGMENTS = 64;
+        const float PI = 3.14159265359;
+        for (unsigned int y = 0; y <= Y_SEGMENTS; ++y)
+        {
+            for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
+            {
+                float xSegment = (float)x / (float)X_SEGMENTS;
+                float ySegment = (float)y / (float)Y_SEGMENTS;
+                float xPos = std::cos(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+                float yPos = std::cos(ySegment * PI);
+                float zPos = std::sin(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+
+                positions.push_back(glm::vec3(xPos, yPos, zPos));
+                uv.push_back(glm::vec2(xSegment, ySegment));
+                normals.push_back(glm::vec3(xPos, yPos, zPos));
+            }
+        }
+
+        bool oddRow = false;
+        for (unsigned int y = 0; y < Y_SEGMENTS; ++y)
+        {
+            if (!oddRow) // even rows: y == 0, y == 2; and so on
+            {
+                for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
+                {
+                    indices.push_back(y * (X_SEGMENTS + 1) + x);
+                    indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                }
+            }
+            else
+            {
+                for (int x = X_SEGMENTS; x >= 0; --x)
+                {
+                    indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                    indices.push_back(y * (X_SEGMENTS + 1) + x);
+                }
+            }
+            oddRow = !oddRow;
+        }
+        indexCount = indices.size();
+
+        std::vector<float> data;
+        for (unsigned int i = 0; i < positions.size(); ++i)
+        {
+            data.push_back(positions[i].x);
+            data.push_back(positions[i].y);
+            data.push_back(positions[i].z);
+            if (uv.size() > 0)
+            {
+                data.push_back(uv[i].x);
+                data.push_back(uv[i].y);
+            }
+            if (normals.size() > 0)
+            {
+                data.push_back(normals[i].x);
+                data.push_back(normals[i].y);
+                data.push_back(normals[i].z);
+            }
+        }
+        glBindVertexArray(sphereVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+        float stride = (3 + 2 + 3) * sizeof(float);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, stride, (void*)(5 * sizeof(float)));
+    }
+
+    glBindVertexArray(sphereVAO);
+    glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
+}
+
+// renderQuad() renders a 1x1 XY quad in NDC
+// -----------------------------------------
+unsigned int quadVAO = 0;
+unsigned int quadVBO;
+void renderQuad()
+{
+    if (quadVAO == 0)
+    {
+        float quadVertices[] = {
+            // positions        // texture Coords
+            -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+             1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+             1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        };
+        // setup plane VAO
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    }
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    glBindVertexArray(0);
+}
